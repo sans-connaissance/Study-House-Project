76c96112acf2db83e38f438e9a3f89d3998805a8
sans-connaissance <davemalicke@gmail.com>
2022-06-29 08:42:10 -0400

    Initial Commit

---
+++ Study-House-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study-House-Project
+//
+//  Created by David Malicke on 6/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
