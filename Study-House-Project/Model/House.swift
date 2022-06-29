bb03279f82c39a972d8c2719d2a9c37d656d9cc3
sans-connaissance <davemalicke@gmail.com>
2022-06-29 13:49:43 -0400

    ADD initial view controller controls

---
+++ Study-House-Project/Model/House.swift
@@ -0,0 +1,13 @@
+//
+//  House.swift
+//  Study-House-Project
+//
+//  Created by David Malicke on 6/29/22.
+//
+
+import Foundation
+class House {
+    var name = ""
+    var address = ""
+    var isNewHouse = true
+}
